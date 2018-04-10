08a3328b94a482af49db0621db9bfec82a5cdcde
AiaRup <aiale88@gmail.com>
2018-04-10 20:08:02 +0300

    fix function remove-post and display-post

--- main.js
+++ main.js
@@ -20,19 +20,25 @@ $(document).ready(function() {
 
 var displayPost = function (arr) {
   $postSection.find('p').remove();
-  var removeButton = '<button>Remove</button>';
+  var removeButton = '<button class="remove btn btn-danger" type="button">Remove</button>';
   for (var i = 0; i < arr.length; i++) {
-    $postSection.append('<p>' + removeButton + ' ' + arr[i].text + ' id: ' + arr[i].id + '</p>');
-    $postSection.find('p:last').addClass('post').attr('data-id', arr[i].id);
-    $postSection.find('p:last button').attr('type', 'button').addClass('remove btn btn-danger');
-    // .on('click', function () {
-    //   $(this).closest('p').remove();
-    // });
+    $postSection.append('<p class="post" data-id="' + arr[i].id + '">' + removeButton + ' ' + arr[i].text + '</p>');
+    // $postSection.find('p:last').addClass('post').attr('data-id', arr[i].id);
+    // $postSection.find('p:last button').attr('type', 'button').addClass('remove btn btn-danger');
   }
 };
 
 $postSection.on('click', 'button.remove', function () {
-  $(this).closest('p').remove();
+  var $post = $(this).closest('p');
+  var id = $post.data().id;
+
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i].id === id) {
+      posts.splice(i, 1);
+    }
+  }
+
+  $post.remove();
 });
 
 
